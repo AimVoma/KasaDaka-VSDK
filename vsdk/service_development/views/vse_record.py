43da9e44577dd9a37ba0ef4c077aedf9b4915ef2
abaart <abaart@gmail.com>
2017-05-17 12:54:39 +0200

    indentation

--- vsdk/service_development/views/vse_record.py
+++ vsdk/service_development/views/vse_record.py
@@ -7,7 +7,7 @@ def record_get_redirect_url(record_element,session):
         return record_element.redirect.get_absolute_url(session)
     else:
         return None
-    
+
 
 
 def record_generate_context(record_element, session, session_id, element_id):
@@ -21,15 +21,14 @@ def record_generate_context(record_element, session, session_id, element_id):
 
 
 def record(request, element_id, session_id):
-
-        record_element = get_object_or_404(MessagePresentation, pk=element_id)
-        session = get_object_or_404(CallSession, pk=session_id)
-        context = record_generate_context(record_element, session, session_id, element_id) 
+    record_element = get_object_or_404(MessagePresentation, pk=element_id)
+    session = get_object_or_404(CallSession, pk=session_id)
+    context = record_generate_context(record_element, session, session_id, element_id)
     if request.method == 'GET':
-        session.record_step(record_element)   
+        session.record_step(record_element)
         return render(request, 'recording_t.xml', context, content_type='text/xml')
     if request.method == 'POST':
-        redirect_url = record_get_redirect_url(record_element,session) 
+        redirect_url = record_get_redirect_url(record_element,session)
         recording = request.FILES['recording']
 	if True:
             offer = models.Offer(Message=recording)
@@ -39,7 +38,7 @@ def record(request, element_id, session_id):
 	    announcement.save()
         return HttpResponseRedirect(redirect_url)
 
-        
+
 def get_voice_fragment_urlc(self, language):
     """
     Returns the url of the audio file of this element, in the given language.
